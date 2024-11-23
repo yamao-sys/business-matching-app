48386123738133ec6f44338a26a879f3e48a2522
yamao-sys <yamaopanku@gmail.com>
2024-11-23 16:00:57 +0900

    会員登録のつなぎこみ

--- frontend/src/pages/sign_up/components/SignUpForm/index.tsx
+++ frontend/src/pages/sign_up/components/SignUpForm/index.tsx
@@ -1,6 +1,8 @@
 import { FC, useState } from 'react';
 import { PhaseType } from '../../types';
 import { SignUpInput } from '../SignUpInput';
+import { SignUpConfirmation } from '../SignUpConfirmation';
+import { SignUpThanks } from '../SignUpThanks';
 
 export const SignUpForm: FC = () => {
   const [phase, setPhase] = useState<PhaseType>('input');
@@ -11,11 +13,9 @@ export const SignUpForm: FC = () => {
       case 'input':
         return <SignUpInput togglePhase={togglePhase} />;
       case 'confirmation':
-        // return <SignUpConfirmation togglePhase={togglePhase} />;
-        return <></>;
+        return <SignUpConfirmation togglePhase={togglePhase} />;
       case 'thanks':
-        // return <SignUpThanks />;
-        return <></>;
+        return <SignUpThanks />;
     }
   };
 
