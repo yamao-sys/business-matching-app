e157360b356e79d0a8b29be51335df9fad3e0992
yamao-sys <yamaopanku@gmail.com>
2024-11-23 15:36:40 +0900

    AuthServiceを会員登録部に限定

--- api_server/app/services/auth_service.go
+++ api_server/app/services/auth_service.go
@@ -1,27 +1,26 @@
 package services
 
 import (
-	"app/dto"
+	"app/generated/auth"
 	models "app/models/generated"
+	"app/validator"
 	"context"
 	"database/sql"
-	"fmt"
-	"log"
-	"time"
+	"io"
+	"strconv"
 
-	"github.com/dgrijalva/jwt-go"
-	"github.com/go-playground/validator/v10"
-	"github.com/labstack/echo/v4"
 	"github.com/volatiletech/sqlboiler/v4/boil"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"golang.org/x/crypto/bcrypt"
+
+	"cloud.google.com/go/storage"
 )
 
 type AuthService interface {
-	SignUp(ctx context.Context, requestParams dto.SignUpRequest) *dto.SignUpResponse
-	SignIn(ctx context.Context, requestParams dto.SignInRequest) *dto.SignInResponse
-	GetAuthUser(ctx echo.Context) (*models.User, error)
-	Getuser(ctx context.Context, id int) *models.User
+	ValidateSignUp(ctx context.Context, request *auth.PostAuthValidateSignUpMultipartRequestBody) error
+	SignUp(ctx context.Context, requestParams auth.PostAuthValidateSignUpMultipartRequestBody) error
+	// SignIn(ctx context.Context, requestParams dto.SignInRequest) *dto.SignInResponse
+	// GetAuthUser(ctx echo.Context) (*models.User, error)
+	// Getuser(ctx context.Context, id int) *models.User
 }
 
 type authService struct {
@@ -32,87 +31,126 @@ func NewAuthService(db *sql.DB) AuthService {
 	return &authService{db}
 }
 
-func (as *authService) SignUp(ctx context.Context, requestParams dto.SignUpRequest) *dto.SignUpResponse {
-	// NOTE: バリデーションチェック
-	validate := validator.New()
-	validationErrors := validate.Struct(requestParams)
-	if validationErrors != nil {
-		return &dto.SignUpResponse{User: models.User{}, Error: validationErrors, ErrorType: "validationError"}
-	}
+func (as *authService) ValidateSignUp(ctx context.Context, request *auth.PostAuthValidateSignUpMultipartRequestBody) error {
+	return validator.ValidateSignUpSupporter(request)
+}
 
-	user := models.User{}
-	user.Name = requestParams.Name
-	user.Email = requestParams.Email
+func (as *authService) SignUp(ctx context.Context, requestParams auth.PostAuthValidateSignUpMultipartRequestBody) error {
+	supporter := models.Supporter{}
+	supporter.FirstName = requestParams.FirstName
+	supporter.LastName = requestParams.LastName
+	supporter.Email = requestParams.Email
+	supporter.FrontIdentification = ""
+	supporter.BackIdentification = ""
+
 	// NOTE: パスワードをハッシュ化の上、Create処理
 	hashedPassword, err := as.encryptPassword(requestParams.Password)
 	if err != nil {
-		return &dto.SignUpResponse{User: user, Error: err, ErrorType: "internalServerError"}
+		return err
 	}
-	user.Password = hashedPassword
-	createErr := user.Insert(ctx, as.db, boil.Infer())
+	supporter.Password = hashedPassword
+	createErr := supporter.Insert(ctx, as.db, boil.Infer())
 	if createErr != nil {
-		log.Fatalln(createErr)
+		return createErr
 	}
 
-	return &dto.SignUpResponse{User: user, Error: nil, ErrorType: ""}
-}
-
-func (as *authService) SignIn(ctx context.Context, requestParams dto.SignInRequest) *dto.SignInResponse {
-	// NOTE: emailからユーザの取得
-	user, err := models.Users(qm.Where("email = ?", requestParams.Email)).One(ctx, as.db)
+	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "メールアドレスまたはパスワードに該当するユーザが存在しません。", Error: nil}
+		return err
+	}
+	defer client.Close()
+
+	if requestParams.FrontIdentification == nil && requestParams.BackIdentification == nil {
+		return nil
 	}
 
-	// NOTE: パスワードの照合
-	if err := as.compareHashPassword(user.Password, requestParams.Password); err != nil {
-		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "メールアドレスまたはパスワードに該当するユーザが存在しません。", Error: nil}
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": user.ID,
-		"exp":     time.Now().Add(time.Hour * 24).Unix(),
-	})
-	// TODO: JWT_SECRETを環境変数に切り出す
-	tokenString, err := token.SignedString([]byte("abcdefghijklmn"))
-	if err != nil {
-		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "", Error: err}
-	}
-	return &dto.SignInResponse{TokenString: tokenString, NotFoundMessage: "", Error: nil}
-}
+	supporter.Reload(ctx, as.db)
+	supporterID := strconv.Itoa(supporter.ID)
 
-func (as *authService) GetAuthUser(ctx echo.Context) (*models.User, error) {
-	// NOTE: Cookieからtokenを取得
-	tokenString, err := ctx.Cookie("token")
-	if err != nil {
-		return &models.User{}, err
-	}
-	// NOTE: tokenに該当するユーザを取得する
-	token, err := jwt.Parse(tokenString.Value, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	bucket := client.Bucket("business_matching_app_dev")
+
+	if requestParams.FrontIdentification != nil {
+		frontIdentificationPath := "supporters/"+supporterID+"/"+requestParams.FrontIdentification.Filename()
+		reader, _ :=requestParams.FrontIdentification.Reader()
+		uploadFrontIdentificationErr := as.uploadIdentification(bucket, frontIdentificationPath, reader)
+		if uploadFrontIdentificationErr != nil {
+			return uploadFrontIdentificationErr
 		}
-
-		return []byte("abcdefghijklmn"), nil
-	})
-	if err != nil {
-		return &models.User{}, fmt.Errorf("failt jwt parse")
+		supporter.FrontIdentification = frontIdentificationPath
 	}
-
-	var userID int
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userID = int(claims["user_id"].(float64))
+	if requestParams.BackIdentification != nil {
+		backIdentificationPath := "supporters/"+supporterID+"/"+requestParams.BackIdentification.Filename()
+		backIdentificationReader, _ :=requestParams.BackIdentification.Reader()
+		uploadBackIdentificationErr := as.uploadIdentification(bucket, backIdentificationPath, backIdentificationReader)
+		if uploadBackIdentificationErr != nil {
+			return uploadBackIdentificationErr
+		}
+		supporter.BackIdentification = backIdentificationPath
 	}
-	if userID == 0 {
-		return &models.User{}, fmt.Errorf("invalid token")
-	}
-	user, err := models.FindUser(context.Background(), as.db, userID)
-	return user, err
+	_, updateIdenfiticationErr := supporter.Update(ctx, as.db, boil.Infer())
+	return updateIdenfiticationErr
 }
 
-func (as *authService) Getuser(ctx context.Context, id int) *models.User {
-	user, _ := models.FindUser(ctx, as.db, id)
-	return user
-}
+// func (as *authService) ValidateSignUp(ctx context.Context, requestParams auth.SupporterSignUpInput) auth.SupporterSignUpResponse {
+// 	//
+// }
+
+// // func (as *authService) SignIn(ctx context.Context, requestParams dto.SignInRequest) *dto.SignInResponse {
+// // 	// NOTE: emailからユーザの取得
+// // 	user, err := models.Users(qm.Where("email = ?", requestParams.Email)).One(ctx, as.db)
+// // 	if err != nil {
+// // 		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "メールアドレスまたはパスワードに該当するユーザが存在しません。", Error: nil}
+// // 	}
+
+// // 	// NOTE: パスワードの照合
+// // 	if err := as.compareHashPassword(user.Password, requestParams.Password); err != nil {
+// // 		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "メールアドレスまたはパスワードに該当するユーザが存在しません。", Error: nil}
+// // 	}
+// // 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+// // 		"user_id": user.ID,
+// // 		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+// // 	})
+// // 	// TODO: JWT_SECRETを環境変数に切り出す
+// // 	tokenString, err := token.SignedString([]byte("abcdefghijklmn"))
+// // 	if err != nil {
+// // 		return &dto.SignInResponse{TokenString: "", NotFoundMessage: "", Error: err}
+// // 	}
+// // 	return &dto.SignInResponse{TokenString: tokenString, NotFoundMessage: "", Error: nil}
+// // }
+
+// // func (as *authService) GetAuthUser(ctx echo.Context) (*models.User, error) {
+// // 	// NOTE: Cookieからtokenを取得
+// // 	tokenString, err := ctx.Cookie("token")
+// // 	if err != nil {
+// // 		return &models.User{}, err
+// // 	}
+// // 	// NOTE: tokenに該当するユーザを取得する
+// // 	token, err := jwt.Parse(tokenString.Value, func(token *jwt.Token) (interface{}, error) {
+// // 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// // 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+// // 		}
+
+// // 		return []byte("abcdefghijklmn"), nil
+// // 	})
+// // 	if err != nil {
+// // 		return &models.User{}, fmt.Errorf("failt jwt parse")
+// // 	}
+
+// // 	var userID int
+// // 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+// // 		userID = int(claims["user_id"].(float64))
+// // 	}
+// // 	if userID == 0 {
+// // 		return &models.User{}, fmt.Errorf("invalid token")
+// // 	}
+// // 	user, err := models.FindUser(context.Background(), as.db, userID)
+// // 	return user, err
+// // }
+
+// // func (as *authService) Getuser(ctx context.Context, id int) *models.User {
+// // 	user, _ := models.FindUser(ctx, as.db, id)
+// // 	return user
+// // }
 
 // NOTE: パスワードの文字列をハッシュ化する
 func (as *authService) encryptPassword(password string) (string, error) {
@@ -123,9 +161,23 @@ func (as *authService) encryptPassword(password string) (string, error) {
 	return string(hash), nil
 }
 
-// NOTE: パスワードの照合
-func (as *authService) compareHashPassword(hashedPassword, requestPassword string) error {
-	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(requestPassword)); err != nil {
+// // // NOTE: パスワードの照合
+// // func (as *authService) compareHashPassword(hashedPassword, requestPassword string) error {
+// // 	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(requestPassword)); err != nil {
+// // 		return err
+// // 	}
+// // 	return nil
+// // }
+
+func (as *authService) uploadIdentification(bucket *storage.BucketHandle, path string, reader io.Reader) error {
+	obj := bucket.Object(path)
+	writer := obj.NewWriter(ctx)
+	// NOTE: ファイルをCloud Storageにコピー
+	if _, err := io.Copy(writer, reader); err != nil {
+		return err
+	}
+	// NOTE: Writerを閉じて完了
+	if err := writer.Close(); err != nil {
 		return err
 	}
 	return nil
