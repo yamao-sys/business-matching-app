cc6e5c21a5a2151d68fb87601a17aea7bf4ffac3
yamao-sys <yamaopanku@gmail.com>
2024-11-23 15:27:48 +0900

    SQLBoilerの設定と初期マイグレーション

--- api_server/app/db/migrations/20241026142206-create_supporters.sql
+++ api_server/app/db/migrations/20241026142206-create_supporters.sql
@@ -1,10 +1,13 @@
 
 -- +migrate Up
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS supporters(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL,
+	front_identification VARCHAR(255) NOT NULL,
+	back_identification VARCHAR(255) NOT NULL,
 	created_at DATETIME NOT NULL,
 	updated_at DATETIME NOT NULL
 );
