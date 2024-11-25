a56752cc189067ec1cde77c04ffbbf30b04ba167
yamao-sys <yamaopanku@gmail.com>
2024-11-25 19:00:08 +0900

    reviewdogが効いていることが確認できたため戻す

--- frontend/src/pages/sign_up/components/SignUpThanks/index.tsx
+++ frontend/src/pages/sign_up/components/SignUpThanks/index.tsx
@@ -5,7 +5,7 @@ export const SignUpThanks: FC = () => {
   return (
     <>
       <SignUpBaseLayout phase='thanks'>
-        <div className='flex justify-center'
+        <div className='flex justify-center'>
           <div>会員登録が完了しました。</div>
           <div>ご登録いただきありがとうございます。</div>
         </div>
