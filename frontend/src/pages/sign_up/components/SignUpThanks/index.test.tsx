088373d5adf2dbfa59f93ae7e80d39e255687ee9
yamao-sys <yamaopanku@gmail.com>
2024-11-25 19:11:55 +0900

    会員登録のサンクスページのテストのタイトル修正

--- frontend/src/pages/sign_up/components/SignUpThanks/index.test.tsx
+++ frontend/src/pages/sign_up/components/SignUpThanks/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { SignUpThanks } from '.';
 
 describe('pages/sign_up/components/SignUpThanks', () => {
-  test('フォームが表示されること', () => {
+  test('サンクスメッセージが表示されること', () => {
     render(<SignUpThanks />);
 
     screen.getByText('会員登録が完了しました。');
